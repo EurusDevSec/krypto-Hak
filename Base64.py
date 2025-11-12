e479e372ccf1640db2feb4aa8902fe80b4e255ad
LeganSar44 <hoangslevan@gmail.com>
2025-11-12 15:28:20 +0700

    Add Base64 encoding functionality for hex string

---
+++ Base64.py
@@ -0,0 +1,9 @@
+
+import base64
+
+hexStr = '72bca9b68fc16ac7beeb8f849dca1d8a783e8acf9679bf9269f7bf'
+
+hexBytes = bytes.fromhex(hexStr)
+
+encodeBase64 = base64.b64encode(hexBytes)
+print(encodeBase64)
